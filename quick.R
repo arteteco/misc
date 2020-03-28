569c2dcd33219c4910f2cdece36cf92d18e8bd20
arteteco <39224568+arteteco@users.noreply.github.com>
2020-03-28 17:30:47 +0100

    Update quick.R
    Added if loops to catch exceptions; improved output

--- quick.R
+++ quick.R
@@ -1,14 +1,35 @@
 #!/usr/bin/env Rscript
 
-# This scripts runs from the linux command line. It takes a single argument, which is the nucleotide GenBank code, and outputs title, caption, article reference and more. 
-# It's damn easy to tweak
-
 library("rentrez")
+
+# For testing purposes
+#   args<-"DQ061874"
 
 args = commandArgs(trailingOnly=TRUE)
 
 r<-entrez_summary(db="nuccore", id=args)
 
-p<-entrez_search(db="pubmed", term=r$title)
+p<-entrez_link(dbfrom="nuccore", id=args, db="all")
 
-cat('Title:', r$title, "\n", 'Organism: ', r$organism, "\n", 'Caption:',r$caption, "\n", 'Tech: ', r$tech, "\n", 'Article: ', p$title, "\n")
+pub<-p$links$nuccore_pubmed
+
+if(is.null(pub)){
+  pub= 'No article found'
+  
+} else {
+  
+  pub=paste('https://www.ncbi.nlm.nih.gov/pubmed/?term=',pub, sep="")
+}
+
+gbl<-paste('https://www.ncbi.nlm.nih.gov/nuccore/', args, sep="")
+
+pubm<-entrez_summary(db="pubmed", id=p$links$nuccore_pubmed)
+
+if(is.null(pubm)){
+  pubm= 'No article found'
+  
+}
+
+auth<-do.call(paste,pubm$authors[1])
+
+cat('Title:', r$title, "\n", 'Organism: ', r$organism, "\n", 'PubMed: ', pubm$title, "\n", 'Authors: ', auth, "\n",pub, "\n", 'GenBank: ', gbl, "\n")
